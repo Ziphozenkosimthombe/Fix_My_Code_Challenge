e535dd886d58964b85466eb370c09d3008573571
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-26 06:53:49 -0700

    challenge

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,33 @@
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next if arg !~ /^-?[0-9]+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+    if i_arg.nil?
+    else
+      # insert result at the right position
+      is_inserted = false
+      i = 0
+      l = result.size
+      while !is_inserted && i < l do
+if result[i] < i_arg
+  i += 1
+else
+  result.insert(i - 1, i_arg)
+  is_inserted = true
+break
+end
+
+end
+      result << i_arg if !is_inserted
+    end
+end
+puts result
